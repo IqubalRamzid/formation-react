2237fc68fffaf37b48c07e036ec047694120f941
iqubal <iqubal.ramzid@outlook.fr>
2022-05-03 10:10:58 +0200

    premiere lecon react

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("application react");
+import React from 'react'
+import { render } from 'react-dom'
+
+const html = (
+  <div>
+    <h2>bonjour</h2>
+
+    <h2>on a réussi </h2>
+
+    <p> c'est sur qu'on a réussi </p>
+  </div>
+)
+
+const div = document.querySelector('#root')
+
+render(html, div)
